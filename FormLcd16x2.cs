fc0dcd04e56dda7b8a4ce4aa309d034dc5c2e9c3
wleng2001 <kamilgrodzki5@gmail.com>
2024-04-14 21:13:47 +0200

    matrix to generate char and char generator works correctly

--- FormLcd16x2.cs
+++ FormLcd16x2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,31 +20,24 @@ namespace display_graphic_generator
 
         private void FormLcd16x2_Load(object sender, EventArgs e)
         {
-
+            btnPutOnForm(sender, e);
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if(char0x0Button.BackColor == Color.Lime)
-            {
-                char0x0Button.BackColor = Color.Gray;
-            }
-            else
-            {
-                char0x0Button.BackColor = Color.Lime;
-            }
-        }
-
         private void button41_Click(object sender, EventArgs e)
         {
             tabContentTextBox.Text = "uint8_t "+tabNameTextBox.Text+"[]={\n";
-            tabContentTextBox.Text = tabContentTextBox.Text + "B" + "000";
-            tabContentTextBox.Text = tabContentTextBox.Text + "}";
+            for(int i = 0; i < lC.Content.Length; i++)
+            {
+                string number = Convert.ToString( lC.Content[i], toBase: 2).PadLeft(8,'0');
+                tabContentTextBox.AppendText(Environment.NewLine);
+                tabContentTextBox.AppendText( "\tB" + number+",");
+            }
+            tabContentTextBox.AppendText(Environment.NewLine);
+            tabContentTextBox.Text +=  "};";
         }
 
         private void tabContentTextBox_TextChanged(object sender, EventArgs e)
@@ -51,65 +45,93 @@ namespace display_graphic_generator
 
         }
 
-        private void char0x1Button_Click(object sender, EventArgs e)
-        {
-            if (char0x1Button.BackColor == Color.Lime)
-            {
-                char0x1Button.BackColor = Color.Gray;
-            }
-            else
-            {
-                char0x1Button.BackColor = Color.Lime;
-            }
-        }
-
-        private void char0x2Button_Click(object sender, EventArgs e)
-        {
-            if (char0x2Button.BackColor == Color.Lime)
-            {
-                char0x2Button.BackColor = Color.Gray;
-            }
-            else
-            {
-                char0x2Button.BackColor = Color.Lime;
-            }
-        }
-
-        private void char0x3Button_Click(object sender, EventArgs e)
-        {
-            if (char0x3Button.BackColor == Color.Lime)
-            {
-                char0x3Button.BackColor = Color.Gray;
-            }
-            else
-            {
-                char0x3Button.BackColor = Color.Lime;
-            }
-        }
-
-        private void char0x4Button_Click(object sender, EventArgs e)
-        {
-            if (char0x4Button.BackColor == Color.Lime)
-            {
-                char0x4Button.BackColor = Color.Gray;
-            }
-            else
-            {
-                char0x4Button.BackColor = Color.Lime;
-            }
-        }
-
         private void tabNameTextBox_TextChanged(object sender, EventArgs e)
         {
             
         }
+
+        private void charMatrixflowLayoutPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        lcdContent lC = new lcdContent(8);
+        byte width = 20;
+        byte height = 20;
+        byte x = 50;
+        byte y = 60;
+        byte takeNumber(string text, byte place)
+        {
+            string number = "";
+            char letter = text[place];
+            do
+            {
+                number += letter;
+                place++;
+                if (place < text.Length)
+                    letter = text[place];
+                else
+                    break;
+            } while (char.IsDigit(letter));
+            return byte.Parse(number);
+        }
+        void bClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            string name = b.Name;
+            byte row = takeNumber(name, 0);
+            byte col = takeNumber(name, (byte)((row.ToString().Length)+1));
+            bool status = !lC.get(row, (byte)(4 - col));
+            lC.set(row, (byte)(4-col), status);
+            if (status == true)
+                b.BackColor = Color.Gray;
+            else
+                b.BackColor = Color.Lime;
+        }
+
+        Button btn(string name, byte width, byte height)
+        {
+            Button b = new Button();
+            b.Name = name;
+            b.Size = new Size(width, height);
+            b.BackColor = Color.Lime;
+            b.FlatStyle = FlatStyle.Flat;
+            b.FlatAppearance.BorderSize = 1;
+            b.FlatAppearance.BorderColor = Color.Green;
+            b.Click += bClick;
+            return b;
+        }
+
+        private void btnPutOnForm(object sender, EventArgs e)
+        {
+            for(byte i = 0; i <8; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    Button b = btn(i.ToString()+" "+j.ToString(), width, height);
+                    b.Location = new Point(y+j*width, x+i*height);
+                    Controls.Add(b);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
     public class lcdContent
     {
-        private byte[] content =
+        private byte[] content;
+        public lcdContent(byte rows)
+        {
+            this.content = new byte[rows];
+
+            for (int i = 0; i < this.content.Length; i++)
             {
-                0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
-            };
+                content[i] = 0;
+            }
+        }
         public byte[] Content
         {
             get { return content; }
@@ -117,7 +139,22 @@ namespace display_graphic_generator
         }
         public void set(byte row, byte col, bool value)
         {
+            byte mask = (byte)(1 << col);
+            if (value == true)
+                content[row] |= mask;
+            else
+                content[row] &= (byte)~mask;
+        }
 
+        public bool get(byte row, byte col)
+        {
+            byte rowValue = content[row];
+            bool value = false;
+            if ((rowValue & ( 1 << col)) > 0)
+                value = true;
+
+            return value;
+            
         }
     };
 }
