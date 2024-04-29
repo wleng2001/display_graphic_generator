4927c9d28bf743bb0f647b83f314357019e43f9f
wleng2001 <kamilgrodzki5@gmail.com>
2024-04-29 21:15:13 +0200

    change appearance of window

--- FormLcd16x2.Designer.cs
+++ FormLcd16x2.Designer.cs
@@ -60,7 +60,7 @@
             this.tabNameTextBox.ImeMode = System.Windows.Forms.ImeMode.Disable;
             this.tabNameTextBox.Location = new System.Drawing.Point(454, 58);
             this.tabNameTextBox.Name = "tabNameTextBox";
-            this.tabNameTextBox.Size = new System.Drawing.Size(334, 27);
+            this.tabNameTextBox.Size = new System.Drawing.Size(333, 27);
             this.tabNameTextBox.TabIndex = 0;
             this.tabNameTextBox.Text = "tabName";
             this.tabNameTextBox.TextChanged += new System.EventHandler(this.tabNameTextBox_Enter);
@@ -71,10 +71,10 @@
             this.tabContentTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(254)))));
             this.tabContentTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tabContentTextBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tabContentTextBox.Location = new System.Drawing.Point(454, 116);
+            this.tabContentTextBox.Location = new System.Drawing.Point(454, 118);
             this.tabContentTextBox.Multiline = true;
             this.tabContentTextBox.Name = "tabContentTextBox";
-            this.tabContentTextBox.Size = new System.Drawing.Size(334, 268);
+            this.tabContentTextBox.Size = new System.Drawing.Size(333, 278);
             this.tabContentTextBox.TabIndex = 1;
             this.tabContentTextBox.TextChanged += new System.EventHandler(this.tabContentTextBox_TextChanged);
             // 
@@ -100,9 +100,9 @@
             // 
             // generateButton
             // 
-            this.generateButton.Location = new System.Drawing.Point(697, 390);
+            this.generateButton.Location = new System.Drawing.Point(682, 404);
             this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(91, 39);
+            this.generateButton.Size = new System.Drawing.Size(105, 39);
             this.generateButton.TabIndex = 4;
             this.generateButton.Text = "generate";
             this.generateButton.UseVisualStyleBackColor = true;
@@ -138,7 +138,7 @@
             this.blueToolStripMenuItem,
             this.greyToolStripMenuItem});
             this.matrixColorToolStripMenuItem.Name = "matrixColorToolStripMenuItem";
-            this.matrixColorToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.matrixColorToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
             this.matrixColorToolStripMenuItem.Text = "matrix color";
             // 
             // redToolStripMenuItem
@@ -178,7 +178,7 @@
             this.toolStripMenuItem3,
             this.toolStripMenuItem5});
             this.quantityOfMatrixToolStripMenuItem.Name = "quantityOfMatrixToolStripMenuItem";
-            this.quantityOfMatrixToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.quantityOfMatrixToolStripMenuItem.Size = new System.Drawing.Size(210, 26);
             this.quantityOfMatrixToolStripMenuItem.Text = "quantity of matrix";
             this.quantityOfMatrixToolStripMenuItem.Click += new System.EventHandler(this.quantityOfMatrixToolStripMenuItem_Click);
             // 
@@ -187,21 +187,21 @@
             this.toolStripMenuItem2.Checked = true;
             this.toolStripMenuItem2.CheckState = System.Windows.Forms.CheckState.Checked;
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(100, 26);
             this.toolStripMenuItem2.Text = "1";
             this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
             // 
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(100, 26);
             this.toolStripMenuItem3.Text = "2";
             this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
             // 
             // toolStripMenuItem5
             // 
             this.toolStripMenuItem5.Name = "toolStripMenuItem5";
-            this.toolStripMenuItem5.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(100, 26);
             this.toolStripMenuItem5.Text = "4";
             this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
             // 
@@ -226,9 +226,9 @@
             // 
             // negativeButton
             // 
-            this.negativeButton.Location = new System.Drawing.Point(576, 390);
+            this.negativeButton.Location = new System.Drawing.Point(568, 404);
             this.negativeButton.Name = "negativeButton";
-            this.negativeButton.Size = new System.Drawing.Size(91, 39);
+            this.negativeButton.Size = new System.Drawing.Size(105, 39);
             this.negativeButton.TabIndex = 3;
             this.negativeButton.Text = "negative";
             this.negativeButton.UseVisualStyleBackColor = true;
@@ -236,9 +236,9 @@
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(454, 390);
+            this.clearButton.Location = new System.Drawing.Point(454, 404);
             this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(91, 39);
+            this.clearButton.Size = new System.Drawing.Size(105, 39);
             this.clearButton.TabIndex = 2;
             this.clearButton.Text = "clear";
             this.clearButton.UseVisualStyleBackColor = true;
